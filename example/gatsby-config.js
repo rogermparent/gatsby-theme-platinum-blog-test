a108516dc1a558613bec29d017b2712bca6ecf2a
rogermparent <rogermparent@gmail.com>
2020-01-09 15:37:32 -0500

    Add manifest with favicon to example site

--- example/gatsby-config.js
+++ example/gatsby-config.js
@@ -1,8 +1,20 @@
 module.exports = {
   plugins: [
+    "gatsby-plugin-netlify-cache",
     "gatsby-theme-platinum-blog",
     "gatsby-plugin-preload-link-crossorigin",
-    "gatsby-plugin-netlify-cache",
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Gatsby Theme: Platinum Blog example`,
+        short_name: `Platinum Blog`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#cccccc`,
+        display: `minimal-ui`,
+        icon: `assets/logo.png` // This path is relative to the root of the site.
+      }
+    },
     "gatsby-plugin-offline"
   ]
 };
